709016bddf883d1d7f78866b588520de1c870a4c
IgorLevyy <i-levyy@mail.ru>
2019-07-04 17:17:16 +0300

    Color
    ColorScript

---
+++ Assets/changeColor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeColor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GetComponent<Renderer>().material.color = GetRandomColor();
+        }
+    }
+
+    private Color GetRandomColor()
+    {
+        return new Color(Random.Range(0, 1f),
+            Random.Range(0, 1f),
+            Random.Range(0, 1f));
+    }
+}
