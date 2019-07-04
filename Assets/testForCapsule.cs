7fe17cd4c39724b886d5c7338cfcb1ca42143322
Сафонов Александр <safonoval@ya.ru>
2019-07-04 17:17:41 +0300

    capsule
    capsule

---
+++ Assets/testForCapsule.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testForCapsule : MonoBehaviour
+{
+    public int A;
+    // Start is called before the first frame update
+    void Start()
+    {
+        A = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        A++;
+    }
+}
